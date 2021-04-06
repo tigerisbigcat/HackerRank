9ee5b09b2e00ef0b997067cddbfcd25ca3184dc3
Hao <xiayushan2003@gmail.com>
2021-04-05 19:40:43 -0700

    updated

--- HashTable.py
+++ HashTable.py
@@ -25,13 +25,10 @@ def checkMagazine(magazine, note):
 
 if __name__ == '__main__':
     mn = ("7 4").split()
-
     m = int(mn[0])
-
     n = int(mn[1])
 
     magazine = ("ive got a lovely bunch of coconuts").rstrip().split()
-
     note = ("ive got some coconuts").rstrip().split()
 
     checkMagazine(magazine, note)
